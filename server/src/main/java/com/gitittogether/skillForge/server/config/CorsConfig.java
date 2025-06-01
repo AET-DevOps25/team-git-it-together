31c4c5113c342684210df5ab0b998d4d2a50b271
GravityDarkLab <labidiachraf6@gmail.com>
2025-06-01 22:10:52 +0200

    feat: implement CORS configuration for handling cross-origin requests

---
+++ server/src/main/java/com/gitittogether/skillForge/server/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.gitittogether.skillForge.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.addAllowedOriginPattern("*");
+        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        cfg.setAllowedHeaders(List.of("*")); // allow all headers
+        cfg.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+        return source;
+    }
+}
