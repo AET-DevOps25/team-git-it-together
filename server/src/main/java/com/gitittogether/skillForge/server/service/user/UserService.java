fe0f1650be4f5ef092a6e3d62314f29a9e53beda
GravityDarkLab <labidiachraf6@gmail.com>
2025-06-01 16:52:11 +0200

    feat: implement user registration, authentication, and profile management methods in UserService

--- server/src/main/java/com/gitittogether/skillForge/server/service/user/UserService.java
+++ server/src/main/java/com/gitittogether/skillForge/server/service/user/UserService.java
@@ -1,4 +1,18 @@
 package com.gitittogether.skillForge.server.service.user;
 
+import com.gitittogether.skillForge.server.dto.request.user.UserLoginRequest;
+import com.gitittogether.skillForge.server.dto.request.user.UserProfileUpdateRequest;
+import com.gitittogether.skillForge.server.dto.request.user.UserRegisterRequest;
+import com.gitittogether.skillForge.server.dto.response.user.UserLoginResponse;
+import com.gitittogether.skillForge.server.dto.response.user.UserProfileResponse;
+import com.gitittogether.skillForge.server.dto.response.user.UserRegisterResponse;
+
 public interface UserService {
+    UserRegisterResponse registerUser(UserRegisterRequest request);
+
+    UserLoginResponse authenticateUser(UserLoginRequest request);
+
+    UserProfileResponse getUserProfile(String userId);
+
+    UserProfileResponse updateUserProfile(String userId, UserProfileUpdateRequest request);
 }
