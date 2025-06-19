cd4a80b56875304712410aed66d5f4bf9cf70a60
GravityDarkLab <labidiachraf6@gmail.com>
2025-06-20 01:34:36 +0200

    fix: Add missing descriptions for EC2 instance ID and public IP outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "instance_id" {
+  description = "ID of the EC2 instance"
   value = aws_instance.ubuntu.id
 }
 
 output "public_ip" {
+  description = "Public IP of the EC2 instance"
   value = data.aws_eip.used.public_ip
 }
