4c2d8edd9c1057e6715892e600f5144f4532ea07
GravityDarkLab <labidiachraf6@gmail.com>
2025-07-02 01:25:30 +0200

    feat(tests): optimize test configuration and performance settings for faster execution in the gateway

--- server/skillforge-gateway/src/main/java/com/gitittogether/skillforge/server/gateway/config/GatewayConfig.java
+++ server/skillforge-gateway/src/main/java/com/gitittogether/skillforge/server/gateway/config/GatewayConfig.java
@@ -8,10 +8,12 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 
 @Slf4j
 @Configuration
+@Profile("!test")
 public class GatewayConfig {
 
     @Bean
