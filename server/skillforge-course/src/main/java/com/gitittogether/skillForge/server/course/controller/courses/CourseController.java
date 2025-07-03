1e2ca5e2a4c21f4895aea78668fcc72c427a29b6
GravityDarkLab <labidiachraf6@gmail.com>
2025-07-03 11:29:39 +0200

    feat: add health check endpoint to CourseController

--- server/skillforge-course/src/main/java/com/gitittogether/skillForge/server/course/controller/courses/CourseController.java
+++ server/skillforge-course/src/main/java/com/gitittogether/skillForge/server/course/controller/courses/CourseController.java
@@ -22,6 +22,18 @@ public class CourseController {
 
     private final CourseService courseService;
 
+    /**
+     * Health check endpoint to verify if the service is running.
+     *
+     * @return ResponseEntity with a simple message indicating the service is running.
+     */
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        log.info("Health check endpoint called");
+        return ResponseEntity.ok("Course service is up and running!");
+    }
+
+
     @PostMapping
     public ResponseEntity<CourseResponse> createCourse(@RequestBody CourseRequest request) {
         log.info("Creating new course: {}", request.getTitle());
