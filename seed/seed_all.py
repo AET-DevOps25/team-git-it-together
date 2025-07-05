feb9c8ea7d4bff64ca45f5e219fa839b629e24e2
mahdibayouli <bayoulimahdi1@gmail.com>
2025-07-05 14:29:50 +0200

    feat: add script to resolve paths for generator and seeder scripts

--- seed/seed_all.py
+++ seed/seed_all.py
@@ -1,17 +1,6 @@
-import subprocess
 import os
 
 # Paths
 script_dir = os.path.dirname(os.path.abspath(__file__))
 generator = os.path.join(script_dir, "generate_seed_data.py")
 seeder = os.path.join(script_dir, "seed_database.py")
-
-# Run generator
-print("🚀 Generating seed data...")
-subprocess.run(["python", generator], check=True)
-
-# Run seeder
-print("🌱 Seeding the database...")
-subprocess.run(["python", seeder], check=True)
-
-print("✅ Done!")
